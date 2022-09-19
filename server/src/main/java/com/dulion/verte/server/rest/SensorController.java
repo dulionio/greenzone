6fe9bbd706e1c50ce3dee81bb7eb693a62a6f554
Phillip DuLion <phillip@dulion.com>
2022-09-18 19:09:27 -0700

    Initial application

---
+++ server/src/main/java/com/dulion/verte/server/rest/SensorController.java
@@ -0,0 +1,8 @@
+package com.dulion.verte.server.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SensorController {
+
+}
---
+++ server/src/test/java/com/dulion/verte/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulion.verte.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
