b291a22dadb17a389ceb6a4a325ec2b3342925f7
PHILLIP DULION <phillip@dulion.com>
2022-09-23 22:51:01 -0700

    Banner and logging

--- server/src/main/java/com/dulion/verte/server/rest/SensorController.java
+++ server/src/main/java/com/dulion/verte/server/rest/SensorController.java
@@ -8,10 +8,13 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/readings")
 public class SensorController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(SensorController.class);
+
+  private final ObjectMapper mapper;
+
   @Autowired
-  private ObjectMapper mapper;
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+  public SensorController(ObjectMapper mapper) {
+    this.mapper = mapper;
+  }
 
   @GetMapping
   public List<Reading> getReadings() {
@@ -50,9 +60,9 @@ public class SensorController {
   }
 
   @PostMapping
-  public ResponseEntity<Void> postReading(Reading value) {
+  public ResponseEntity<Void> postReading(@RequestBody Reading value) {
     try {
-      System.out.println(mapper.writeValueAsString(value));
+      LOG.info(mapper.writeValueAsString(value));
     } catch (JsonProcessingException e) {
       return ResponseEntity.badRequest().build();
     }
