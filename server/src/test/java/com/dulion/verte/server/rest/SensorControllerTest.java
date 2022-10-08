835f629419644e95c981f3fd8b7c14d78d9c55b3
PHILLIP DULION <phillip@dulion.com>
2022-10-08 15:54:56 -0700

    Update test

--- server/src/test/java/com/dulion/verte/server/rest/SensorControllerTest.java
+++ server/src/test/java/com/dulion/verte/server/rest/SensorControllerTest.java
@@ -1,10 +1,14 @@
 package com.dulion.verte.server.rest;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.dulion.verte.server.data.Reading;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -24,9 +28,15 @@ public class SensorControllerTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(SensorControllerTest.class);
 
+    private final MockMvc mockMvc;
+    private final ObjectMapper mapper;
+
     @Autowired
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    private MockMvc mockMvc;
+    public SensorControllerTest(MockMvc mockMvc, ObjectMapper mapper) {
+        this.mockMvc = mockMvc;
+        this.mapper = mapper;
+    }
 
     @Test
     public void whenGetList() throws Exception {
@@ -38,26 +48,34 @@ public class SensorControllerTest {
     @Test
     public void whenPost() throws Exception {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-        String json = ("{'sensorId':'"
-            + UUID.randomUUID()
-            + "','dateTime':'"
-            + ZonedDateTime.now(ZoneOffset.UTC).format(formatter)
-            + "','temperature':"
-            + "24.3"
-            + ",'humidity':"
-            + "74.4"
-            + ",'pressure':"
-            + "1000.4"
-            + "}").replace('\'', '"');
+        UUID uuid = UUID.randomUUID();
+        String json = (
+            "{'sensorId':'" + uuid
+                + "','dateTime':'" + ZonedDateTime.now(ZoneOffset.UTC).format(formatter)
+                + "','temperature':" + "24.3"
+                + ",'humidity':" + "74.4"
+                + ",'pressure':" + "1004.5}")
+            .replace('\'', '"');
         LOG.info(json);
+
         mockMvc.perform(post("/api/readings")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
             .andDo(print())
             .andExpect(status().isOk());
-        // And get
-        mockMvc.perform(get("/api/readings"))
+
+        String content = mockMvc.perform(get("/api/readings"))
             .andDo(print())
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        LOG.info(content);
+        Reading[] readings = mapper.createParser(content).readValueAs(Reading[].class);
+        assertEquals(readings.length, 1);
+        assertEquals(readings[0].getSensorId(), uuid);
+        assertEquals(readings[0].getTemperature(), new BigDecimal("24.3"));
+        assertEquals(readings[0].getHumidity(), new BigDecimal("74.4"));
+        assertEquals(readings[0].getPressure(), new BigDecimal("1004.5"));
     }
 }
