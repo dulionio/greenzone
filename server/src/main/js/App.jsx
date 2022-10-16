815e3b86bfabef3ffcde02405d22c0d3e71ec374
Phillip DuLion <phillip@dulion.com>
2022-10-16 13:31:49 -0700

    Fix shrinking

--- server/src/main/js/App.jsx
+++ server/src/main/js/App.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { MantineProvider, AppShell, Navbar, Header, ColorSchemeProvider} from '@mantine/core';
+import { MantineProvider, AppShell, Navbar, Header, ColorSchemeProvider, MediaQuery, Burger } from '@mantine/core';
 
 import SiteHeader from './components/SiteHeader';
 import SiteNavbar from './components/SiteNavbar';
@@ -8,6 +8,7 @@ import Content from "./views/Content";
 
 export default function App() {
   const [colorScheme, setColorScheme] = useState('light');
+  const [opened, setOpened] = useState(false);
   const toggleColorScheme = (value) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
@@ -15,8 +16,24 @@ export default function App() {
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
         <AppShell
-          header={<Header height={47} p="xs"><SiteHeader /></Header>}
-          navbar={<Navbar width={{ base: 47 }} p="xs"><SiteNavbar /></Navbar>}>
+          navbarOffsetBreakpoint="sm"
+          navbar={
+            <Navbar p="sm" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 47, lg: 300 }}>
+              <SiteNavbar />
+            </Navbar>
+          }
+          header={
+            <Header p="sm" height={47} >
+              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((opened) => !opened)}
+                  size="sm"
+                  mr="xl" />
+              </MediaQuery>
+              <SiteHeader />
+            </Header>
+          }>
           <Content />
         </AppShell>
       </MantineProvider>
