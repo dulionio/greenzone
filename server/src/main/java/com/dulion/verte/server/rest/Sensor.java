d7d6b11eca2fb0717ad3fd9f0623657b4b441763
PHILLIP DULION <phillip@dulion.com>
2022-12-17 13:09:12 -0800

    Web dependency with spring boot security

--- server/src/main/java/com/dulion/verte/server/rest/Sensor.java
+++ server/src/main/java/com/dulion/verte/server/rest/Sensor.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/sensor")
 public class Sensor {
 
     private static final Logger LOG = LoggerFactory.getLogger(Sensor.class);
